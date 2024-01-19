599ed660ec562443d423da90ac50e3a0fbadb88e
unknown <Monirul-Jim>
2024-01-19 19:14:35 +0600

    added keyframes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import RecapAnimation from "./RecapAnimation";
 const App = () => {
   return (
-    <div className="main">
-      {/* <div className="box-1"></div>
+    <>
+      <div className="main">
+        {/* <div className="box-1"></div>
       <div className="box-container-2">
         <div className="box-2"></div>
       </div> */}
-      <button className="btn">Hover Me</button>
-    </div>
+        <button className="btn">Hover Me</button>
+        <RecapAnimation />
+      </div>
+    </>
   );
 };
 
---
+++ src/RecapAnimation.tsx
@@ -0,0 +1,11 @@
+import "./RecapAnimation.css";
+const RecapAnimation = () => {
+  return (
+    <div>
+      <h1>This component i make learn keyframes</h1>
+      <div className="box"></div>
+    </div>
+  );
+};
+
+export default RecapAnimation;
